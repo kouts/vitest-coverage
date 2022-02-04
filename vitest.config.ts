b3463b2fe87d0a4a8429ef92d7f8cce79829e515
kouts <koutsaftakis@gmail.com>
2022-02-04 18:20:07 +0200

    first commit

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    Vue(),
+  ],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    coverage: {
+      all: true
+    }
+  },
+})
